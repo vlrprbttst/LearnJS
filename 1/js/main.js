c505c3b9d991a9a3c2553d528c8f58e8cba38d03
Valerio <omegaiori@gmail.com>
2018-01-11 18:27:35 +0100

    first

---
+++ 1/js/main.js
@@ -0,0 +1,21 @@
+console.log('i am a string' + ' yeah!')
+console.log(10%2) // se divido 10/2 qual'è il resto? è zero
+
+let theBool = true
+theBool = false  // con let puoi cambiare valore ad una variable
+
+const myConstant = 'variabile immutabile'
+console.log(myConstant)
+
+console.log(theBool)
+
+function logger () {
+  console.log('logged')
+}
+logger()
+
+function add (number1, number2) {
+  console.log('prima:' + (number1 + number2) + ' seconda:' + (number1 * number2))
+}
+
+add(2, 5)
